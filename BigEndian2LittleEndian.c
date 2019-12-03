701108493dd18c811c2697e5265738dd83224e80
Michael McGuire <embedded.engineer@outlook.com>
2019-12-03 15:43:58 -0800

    noticed strange formatting within github
    using tabs before comments

--- BigEndian2LittleEndian.c
+++ BigEndian2LittleEndian.c
@@ -14,14 +14,14 @@ int main()
     int BigEndian_int = 0xAABBCCDD;
     int LittleEndian_int = 0x00000000;
    
-   	// using shift registers and bitwise operators, we shift, single out the corresponding bit, and reallign them to our new variable in reverse order.  This example is a little tricky, as it is using bitwise left and bitwise right, which is not recommended for production code {KISS}  
+	// using shift registers and bitwise operators, we shift, single out the corresponding bit, and reallign them to our new variable in reverse order.  This example is a little tricky, as it is using bitwise left and bitwise right, which is not recommended for production code {KISS}  
     LittleEndian_int = ((BigEndian_int>>24)&0xff) | ((BigEndian_int>>8)&0xff00) | ((BigEndian_int<<8)&0xff0000) | ((BigEndian_int<<24)&0xff000000);
  
- 	// sending results to the console using printf (stdout iostream)
+	// sending results to the console using printf (stdout iostream)
     printf("Result from Endian conversion\n\n");
     printf("Big Endian = %X\n",BigEndian_int);
     printf("Little Endian = %X\n\n\n",LittleEndian_int);
     
-    // exit main
+	// exit main
     return 0;
 }
